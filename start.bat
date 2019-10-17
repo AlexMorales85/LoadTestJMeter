85d46ec34e6f9e5c0ec77fa3b79a3269146f800a
Alejandro Morales Acedo <amorales@plainconcepts.com>
2019-10-17 09:30:31 +0200

    Use docker-compose

--- start.bat
+++ start.bat
@@ -1,8 +1,1 @@
-docker volume create jenkins
-docker network create jenkins
-
-docker build -t jenkinswithjmeter .
-docker pull httpd
-
-docker run -d --name web --network jenkins -p 80:80 httpd
-docker run --name jenkins --network jenkins -p 8080:8080 -p 50000:50000 -v jenkins:/var/jenkins_home jenkinswithjmeter
+docker-compose up
